baff3da29040d492f6810776e9e7857709449ffc
wanditi kamau <stanley@aigent.com>
2021-06-23 07:43:46 +0200

    Initial commit

---
+++ Tacx.Activities.Api/Controllers/ActivitiesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tacx.Activities.Api.Models;
+using Tacx.Activities.Api.Repository;
+using Tacx.Activities.Api.Services;
+
+namespace Tacx.Activities.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActivitiesController : ControllerBase
+    {
+        private readonly ActivitiesRepository _activitiesRepository;
+        private readonly FilesStorageService _filesStorageService;
+        public ActivitiesController()
+        {
+            _activitiesRepository = new ActivitiesRepository();
+            _filesStorageService = new FilesStorageService();
+        }
+
+        [HttpPost]
+        public IActionResult Create()
+        {            
+            var newActivity = new ActivityDto();
+
+            return Ok(newActivity);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetActivityById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteActivity(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tacx.Activities.Api/Services/FilesStorageService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tacx.Activities.Api.Services
+{
+    public class FilesStorageService
+    {
+        public FilesStorageService()
+        {
+
+        }
+
+        public bool SaveFile(Stream file, string fileName, bool overwrite = false)
+        {
+            return true;
+        }
+    }
+}
